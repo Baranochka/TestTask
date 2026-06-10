91cface5a8aafa887d36f7ea2a70bb9bb2bef838
Baranochka <Baranochka@users.noreply.github.com>
2026-06-11 01:40:13 +0300

    Add read csv

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import csv
+import ast
+
+def read_csv_file() -> list:
+    data = []
+    with open("player_stalls.csv") as csvfile:
+        csvdata = csv.reader(csvfile, delimiter=';')
+        for rows in csvdata:
+            row = []
+            if rows[0] in ['player_1','player_2']:
+                row.append(rows[0])
+                row.append(int(rows[1]))
+                row.append(ast.literal_eval(rows[2]))
+                temp = rows[3][:rows[3].index(']') + 1]
+                row.append(ast.literal_eval(temp))
+                data.append(row)
+    return data
 
 def main():
-    print("Hello from vk!")
+    data = read_csv_file()
 
 
 if __name__ == "__main__":
